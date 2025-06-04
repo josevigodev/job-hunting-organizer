0c0771db794bd1d00272d31184dd4eb943b0e288
Jose Vigo <josevigodev@gmail.com>
2025-06-03 21:49:18 -0700

    changing card, column, and header styles

--- src/components/JobCard/JobCard.jsx
+++ src/components/JobCard/JobCard.jsx
@@ -29,48 +29,50 @@ export function JobCard({
   const { deleteJob } = useContext(JobsContext);
 
   return (
-    <article className={`${styles.job_card} ${styles[state]}`}>
+    <>
       <JobForm edit job={jobData} active={active} setActive={setActive} />
-      <div className={styles.header}>
-        <h3 className={styles.title}>{company}</h3>
-      </div>
-      <h4>{job}</h4>
-      <div className={styles.info}>
-        <span>
-          <CoinIcon />
-        </span>
-        <span>${salary} / month</span>
-      </div>
-
-      <div className={styles.info}>
-        <span>
-          <CalendarIcon />
-        </span>
-        <span>{date}</span>
-      </div>
-      <div className={styles.info}>
-        <span>
-          <MonitorIcon />
-        </span>
-        <span>{workplace}</span>
-      </div>
-
-      <p className={styles.description}>{description}</p>
-
-      <div className={styles.actions}>
-        <Link to={link} state={state}>
-          <span>Go</span>
+      <article className={`${styles.job_card} ${styles[state]}`}>
+        <div className={styles.header}>
+          <h3 className={styles.title}>{company}</h3>
+        </div>
+        <h4>{job}</h4>
+        <div className={styles.info}>
           <span>
-            <LinkIcon />
+            <CoinIcon />
           </span>
-        </Link>
-        <Button handleClick={() => setActive('form')} type='edit'>
-          <EditIcon />
-        </Button>
-        <Button handleClick={() => deleteJob(id)} type='delete'>
-          <TrashIcon />
-        </Button>
-      </div>
-    </article>
+          <span>${salary} / month</span>
+        </div>
+
+        <div className={styles.info}>
+          <span>
+            <CalendarIcon />
+          </span>
+          <span>{date}</span>
+        </div>
+        <div className={styles.info}>
+          <span>
+            <MonitorIcon />
+          </span>
+          <span>{workplace}</span>
+        </div>
+
+        <p className={styles.description}>{description}</p>
+
+        <div className={styles.actions}>
+          <Link to={link} state={state}>
+            <span>Go</span>
+            <span>
+              <LinkIcon />
+            </span>
+          </Link>
+          <Button handleClick={() => setActive('form')} type='edit'>
+            <EditIcon />
+          </Button>
+          <Button handleClick={() => deleteJob(id)} type='delete'>
+            <TrashIcon />
+          </Button>
+        </div>
+      </article>
+    </>
   );
 }
